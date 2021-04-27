c99b15f6c867669068ad961ca9c5b76b990593bb
Nouran  Ayman  Mohamed <nouraaan.aymaaan@gmail.com>
2021-04-28 00:32:58 +0200

    Auto Switch Between Fields

---
+++ GameShowIdioms/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public int CurrentFieldIndex = 0;
+
+    public void CheckForFieldUpdate()
+    {
+        if (IdiomsGameManager.Instance.CurrentIdiom.Words[CurrentFieldIndex].InputField.text.Length == IdiomsGameManager.Instance.CurrentIdiom.Words[CurrentFieldIndex].InputField.characterLimit)
+        {
+            //Debug.Log("Done !");
+
+            //Update CurrentFieldIndex.
+            CurrentFieldIndex++;
+
+            //Select CurrentFieldIndex.
+            if(CurrentFieldIndex < IdiomsGameManager.Instance.CurrentIdiom.Words.Count)
+               IdiomsGameManager.Instance.CurrentIdiom.Words[CurrentFieldIndex].InputField.Select();
+        }
+    }
+}
